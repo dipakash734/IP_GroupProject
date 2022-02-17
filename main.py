d4a86bbee87c38206c696c52e270c6f7707395f2
EthanLee <68798299+Etherealee@users.noreply.github.com>
2022-02-17 22:09:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#Menu page
+print('Welcome to battle game! \n(1)new game\n(2)save game\n(3)quit')
 
+def main():
+    selection = input('Choose your option')
+    if selection.upper() == '1':
+        new_game()              
+    elif selection.upper() == '2':
+        print('Loading save game...')
+        pass
+    elif selection.upper() == '3':
+        print('Exit game...')
+        pass
+    else:
+        print("You entered the wrong option")
+        return main()
