ec62da108f5b9558ead0b677194829271912a35b
EthanLee <68798299+Etherealee@users.noreply.github.com>
2022-02-18 13:05:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 
+#Menu
+
+
+#Character Damage
+import random
+
+def damage(c1, c2):
+    dam = 0
+    if c1=="Wizard":
+        dam -= random.randint(1,10)
+    else:
+        dam -= random.randint(5,15)
+    if c2=="Wizard":
+        dam += random.randint(5,20)
+    else:
+        dam += random.randint(1,10)
+    return dam
+
+def defence(c1):
+    if c1=="Wizard":
+        df = random.randint(1,10)
+    else:
+        df = random.randint(5,15)
+    return df
+
+# Player Team
+name1 = input("Enter your team name:")
+print('''
+-----------------------------
+|     Select your team:     |
+|1. Wizard - Tanker - Wizard|
+|2. Tanker - Wizard - Tanker|
+-----------------------------
+''')
+x=int(input())
+if x==1:
+    team1 = {"Wizard1":[100,0], "Tanker1":[100,0], "Wizard2":[100,0]}
+else:
+    team1 = {"Tanker1":[100,0], "Wizard1":[100,0], "Tanker2":[100,0]}
+
+# AI Team
+
+# GamePlay
+
+# scoreboard
